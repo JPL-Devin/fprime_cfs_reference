9e287f07ccaa1300c7539970a2dbe3b240cafd22
M Starch <LeStarch@googlemail.com>
2026-02-17 13:52:58 -0800

    Renaming to fprime_app to fit in standard CFS app name sizing

--- mission_build.cmake
+++ mission_build.cmake
@@ -1,6 +1,6 @@
 ###########################################################
 #
-# FPRIME_REFERENCE_APP mission build setup
+# FPRIME_APP mission build setup
 #
 # This file is evaluated as part of the "prepare" stage
 # and can be used to set up prerequisites for the build,
@@ -8,20 +8,20 @@
 #
 ###########################################################
 
-# The list of header files that control the FPRIME_REFERENCE_APP configuration
-set(FPRIME_REFERENCE_APP_MISSION_CONFIG_FILE_LIST
-  fprime_reference_app_fcncode_values.h
-  fprime_reference_app_interface_cfg_values.h
-  fprime_reference_app_mission_cfg.h
-  fprime_reference_app_perfids.h
-  fprime_reference_app_msg.h
-  fprime_reference_app_msgdefs.h
-  fprime_reference_app_msgstruct.h
-  fprime_reference_app_tbl.h
-  fprime_reference_app_tbldefs.h
-  fprime_reference_app_tblstruct.h
-  fprime_reference_app_topicid_values.h
+# The list of header files that control the FPRIME_APP configuration
+set(FPRIME_APP_MISSION_CONFIG_FILE_LIST
+  fprime_app_fcncode_values.h
+  fprime_app_interface_cfg_values.h
+  fprime_app_mission_cfg.h
+  fprime_app_perfids.h
+  fprime_app_msg.h
+  fprime_app_msgdefs.h
+  fprime_app_msgstruct.h
+  fprime_app_tbl.h
+  fprime_app_tbldefs.h
+  fprime_app_tblstruct.h
+  fprime_app_topicid_values.h
 )
 
-generate_configfile_set(${FPRIME_REFERENCE_APP_MISSION_CONFIG_FILE_LIST})
+generate_configfile_set(${FPRIME_APP_MISSION_CONFIG_FILE_LIST})
 
--- unit-test/common/eventcheck.c
+++ unit-test/common/eventcheck.c
@@ -17,7 +17,7 @@
  ************************************************************************/
 
 /*
-** File: coveragetest_fprime_reference_app.c
+** File: coveragetest_fprime_app.c
 **
 ** Purpose:
 ** Coverage Unit Test cases for the SAMPLE Application
